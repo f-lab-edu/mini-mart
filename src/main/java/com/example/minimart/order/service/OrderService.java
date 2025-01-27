ea910197696151a9e3ae78dca7626393b9580db4
ifi999 <kkwkjm4231@gmail.com>
2025-01-27 19:47:01 +0900

    feat: add order service and service unit tests

---
+++ src/main/java/com/example/minimart/order/service/OrderService.java
@@ -0,0 +1,59 @@
+package com.example.minimart.order.service;
+
+import com.example.minimart.order.controller.dto.request.CreateOrderRequest;
+import com.example.minimart.order.repository.OrderItemJpaRepository;
+import com.example.minimart.order.repository.OrderJpaRepository;
+import com.example.minimart.order.repository.entity.Order;
+import com.example.minimart.order.repository.entity.OrderItem;
+import com.example.minimart.order.repository.entity.OrderStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service
+public class OrderService {
+
+    private final OrderJpaRepository orderJpaRepository;
+    private final OrderItemJpaRepository orderItemJpaRepository;
+
+    public OrderService(OrderJpaRepository orderJpaRepository, OrderItemJpaRepository orderItemJpaRepository) {
+        this.orderJpaRepository = orderJpaRepository;
+        this.orderItemJpaRepository = orderItemJpaRepository;
+    }
+
+    public Order getOrder(Long id) {
+        return orderJpaRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다. 주문 ID: " + id));
+    }
+
+    public void createOrder(CreateOrderRequest request) {
+        Order order = new Order(
+            request.getCustomerId(),
+            request.getTotalPrice(),
+            OrderStatus.PENDING
+        );
+
+        Order savedOrder = orderJpaRepository.save(order);
+        Long orderId = savedOrder.getId();
+
+        List<OrderItem> orderItems = request.getItems().stream()
+            .map(item -> new OrderItem(
+                orderId,
+                item.getProductId(),
+                item.getProductName(),
+                item.getOption(),
+                item.getPrice(),
+                item.getQuantity()
+            ))
+            .toList();
+
+        orderItemJpaRepository.saveAll(orderItems);
+    }
+
+    public List<Order> listOrders() {
+        return orderJpaRepository.findAll();
+    }
+
+}
